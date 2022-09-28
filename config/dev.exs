a05fda186fb9e6c69a9072411e6a87b1052b9347
zahra <zahra_oma@yahoo.com>
2022-09-28 17:49:46 +0330

    Add port 5433

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :tasks, Tasks.Repo,
   password: "mysecretpassword",
   hostname: "localhost",
   database: "test_dev",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
